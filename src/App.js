6dff479a50c10a3a376fcf4a8afe448eca6694f1
Ritika <ritikag1996.rbl@gmail.com>
2023-09-18 13:09:31 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Customer from './components/Customer';
+import Footer from './components/Footer';
+import History from './components/History';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Transfer from './components/Transfer';
+import Currentbalance from './components/Currentbalance';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+     <Navbar/>
+     <Routes>
+      <Route exact path="/" element={<Home/>}></Route>
+      <Route exact path="/history" element={<History/>}></Route>
+      <Route exact path="/customer" element={<Customer/>}></Route>
+      <Route exact path="/transfer/:id" element={<Transfer/>}></Route>
+      <Route exact path="/balance" element={<Currentbalance/>}></Route>
+      <Route exact path="/about" element={<About/>}></Route>
+     </Routes>
+        <Footer/>
+     </Router>
     </div>
   );
 }
